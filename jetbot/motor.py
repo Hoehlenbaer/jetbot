ac49aa967f0f459b8ab35cfbd1cc94849077fe89
santaimpersonator <36016723+santaimpersonator@users.noreply.github.com>
2019-06-26 17:38:39 -0600

    Modification for SparkFun Parts

--- jetbot/motor.py
+++ jetbot/motor.py
@@ -1,5 +1,4 @@
 import atexit
-from Adafruit_MotorHAT import Adafruit_MotorHAT
 import traitlets
 from traitlets.config.configurable import Configurable
 
@@ -16,8 +15,8 @@ class Motor(Configurable):
         super(Motor, self).__init__(*args, **kwargs)  # initializes traitlets
 
         self._driver = driver
-        self._motor = self._driver.getMotor(channel)
         atexit.register(self._release)
+        self.channel = channel
         
     @traitlets.observe('value')
     def _observe_value(self, change):
@@ -25,14 +24,25 @@ class Motor(Configurable):
 
     def _write_value(self, value):
         """Sets motor value between [-1, 1]"""
-        mapped_value = int(255.0 * (self.alpha * value + self.beta))
-        speed = min(max(abs(mapped_value), 0), 255)
-        self._motor.setSpeed(speed)
-        if mapped_value < 0:
-            self._motor.run(Adafruit_MotorHAT.FORWARD)
-        else:
-            self._motor.run(Adafruit_MotorHAT.BACKWARD)
+        speed = int(245 * (self.alpha * value + self.beta))
 
+	# Set Motor Controls: .setDrive( motor number, direction, speed)
+	# Motor Number: A = 0, B = 1
+	# Direction: FWD = 0, BACK = 1
+	# Speed: 0 - 255
+
+	# Issue relating to H-Bridge driver: When setting speed levels, the maximum should be
+	# 255, but in practical use, there are occasions when the maximum input is lower than 255.
+	# In those circumstances, the motor reverses direction once that "practical" maximum input
+	# is exceeded (even if it is below 255) due to how the firmware operates. Therefore, we
+	# will use 245 to keep a margin of error.
+
+        if self.channel == 1:
+            self._motor = self._driver.setDrive(self.channel-1, 0, speed)
+        elif self.channel == 2:
+            self._motor = self._driver.setDrive(self.channel-1, 0, speed)
+        self._driver.enable()
+            
     def _release(self):
         """Stops motor by releasing control"""
-        self._motor.run(Adafruit_MotorHAT.RELEASE)
+        self._motor.disable()
