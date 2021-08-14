62dac1f4b675a95d2ad4a1eeaca24c9fc5353179
Hoehlenbaer <81979625+Hoehlenbaer@users.noreply.github.com>
2021-08-14 13:55:36 +0800

    Update enable.sh

--- docker/enable.sh
+++ docker/enable.sh
@@ -1,7 +1,8 @@
 source configure.sh
 
 JUPYTER_WORKSPACE=${1:-$HOME}  # default to $HOME
-JETBOT_CAMERA=${2:-opencv_gst_camera}  # default to opencv
+#JETBOT_CAMERA=${2:-opencv_gst_camera}  # default to opencv
+JETBOT_CAMERA=${2:-zmq_camera}		# default to zmq_camera
 
 if [ "$JETBOT_CAMERA" = "zmq_camera" ]
 then
